ac3cf3ddd1f7e110b58111d015e7b21ad5c254b0
alexkasiot <alex_kasiot@hotmail.com>
2025-07-26 22:02:12 +0200

    connected the database to the backend server, left a simple test in the index.ts of the backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,22 +1,40 @@
 /// <reference path="../../../packages/db/types/fastify.d.ts" />
 
+import { config } from 'dotenv'
+import { buildServer } from './server';
+import { usersTable } from '@repo/db';
+import path from 'path';
 
-import { buildServer } from "./server";
-// import { usersTable } from "@repo/db";
-
-
+config({ path: path.resolve(__dirname, '../../../.env') });
 
 const start = async () => {
   try {
     const fastify = await buildServer();
 
-  // try {
-    //   await fastify.db.select().from(usersTable).limit(1); // Test query
-    //   console.log('✅ Database connection verified!');
-    // } catch (dbErr: any) {
-    //   console.error('❌ Database test failed:', dbErr.message);
-    // }
+  try {
+      const tableInfo = await fastify.db.run('PRAGMA table_info(users_table)');
+      console.log('Table info:', tableInfo);
 
+      const existingUsers = await fastify.db.select().from(usersTable);
+      console.log('Existing users:', existingUsers);
+    
+      const user = {
+        name: `User.${Date.now()}`,
+        alias: `Johnny.${Date.now()}`,
+        email: `john@example.com.${Date.now()}`,
+      };
+      const result = await fastify.db.insert(usersTable).values(user).returning();
+      console.log('New user created: ' ,result);
+      const users = await fastify.db.select().from(usersTable);
+      console.log('Getting all users from the database: ', users)
+      console.log('✅ Database connection verified!');
+    } catch (dbErr: any) {
+      console.error('Error details:', {
+        message: dbErr.message,
+        code: dbErr.code,
+        detail: dbErr.detail
+    });
+  }
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
     console.log("🚀 Server running at http://localhost:3000");
   } catch (err) {
--- packages/db/src/fastify/db-connector.ts
+++ packages/db/src/fastify/db-connector.ts
@@ -2,7 +2,9 @@ import 'dotenv/config';
 import fastifyPlugin from 'fastify-plugin'
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client'
 import * as schema from '../db/schema'
+import path from 'path';
 
 /**
  * Database connector plugin for SQLite using Drizzle ORM
@@ -11,7 +13,14 @@ import * as schema from '../db/schema'
  */
 
 async function fastifydbConnector(fastify: FastifyInstance, _options: FastifyPluginOptions) {
-	const db = drizzle(process.env.DB_FILE_NAME!, { schema });
+	
+  const dbFileName = process.env.DB_FILE_NAME! || 'local.db';
+  const dbPath = path.resolve(__dirname, '../../../../', dbFileName);
+  console.log("path in dbConnector: ", dbPath);
+  const client = createClient({
+    url: `file:${dbPath}`
+  });
+  const db = drizzle(client, { schema });
 	fastify.decorate('db', db);
 }
 
