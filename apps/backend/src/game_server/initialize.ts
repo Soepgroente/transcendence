6bed49893b7e6011f78141f6ac436064474f9593
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2025-10-28 16:44:10 +0100

    saving for merging

--- apps/backend/src/game_server/initialize.ts
+++ apps/backend/src/game_server/initialize.ts
@@ -13,7 +13,7 @@ export function	createSurroundingWalls(scene: Scene, walls: Wall[], dimensions: 
 
 	/* | on right */
 	walls.push(
-		new Wall(new Vector3(wallThickness, wallHeight, height + wallThickness * 2),
+		new Wall(new Vector3(height, wallHeight, wallThickness),
 		new Vector3(-width / 2 - wallThickness / 2, wallHeight / 2, 0),
 		new Vector3(1, 0, 0),
 		scene)
@@ -21,7 +21,7 @@ export function	createSurroundingWalls(scene: Scene, walls: Wall[], dimensions: 
 
 	/* | on left */
 	walls.push(
-		new Wall(new Vector3(wallThickness, wallHeight, height + wallThickness * 2),
+		new Wall(new Vector3(height, wallHeight, wallThickness),
 		new Vector3(width / 2 + wallThickness / 2, wallHeight / 2, 0),
 		new Vector3(-1, 0, 0),
 		scene)
